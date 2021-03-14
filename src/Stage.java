70286d16b9a56b2c56662c5909c0d3f28e5f8faf
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-03-14 15:15:15 +0000

    transit stage duration based on distance between destination and earth

--- src/Stage.java
+++ src/Stage.java
@@ -4,15 +4,28 @@ public class Stage {
     Mission mission;
     String stages[] = {"prelaunch", "boost", "transit", "landing", "exploration"};
     Integer currentStage;
-    Integer duration[] = {10, 10, 10, 10, 10,};
+    Integer duration[] = {2, 2, 10, 2, 10,};
     Instant stageStartTime;
+    Integer transitDistance;
 
     Stage(Mission mission){
         this.mission = mission;
         this.currentStage = 0;
         this.stageStartTime = Instant.now();
+        this.transitDistance = settransitDistance();
+        setTransitDuration();
     }
 
+    private void setTransitDuration() {
+        Integer transitTime = transitDistance/10000000;
+        duration[2] = transitTime;
+    }
+
+    private Integer settransitDistance(){
+        Celestial destination = mission.destination;
+        Celestial source = mission.source;
+        return (int) destination.slDistance(source);
+    }
     public void incrementStage(){
         if (currentStage <= 3){
             currentStage++;
