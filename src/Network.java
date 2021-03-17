5a64173db6bbe0761eab8cb8afe7eedbd0fcdb55
Paul Treanor <treanorpaul9@gmail.com>
2021-03-17 19:09:08 +0000

    unsttttable

--- src/Network.java
+++ src/Network.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
+
 
 public class Network extends Thread{
 	Controller controller;
@@ -36,13 +38,28 @@ public class Network extends Thread{
                 
                 if (dataTransmission.arrivalTime < this.mission.clock.getTicks()){
                     eventLog.writeFile("Sending " + dataTransmission.getType() + " across network " + choosenConnection);
-                
+                    Boolean connected = false;
+                    while(!connected){
+                        connected = transmitAttempt(dataTransmission, choosenConnection);
+                    } 
                     choosenConnection.sendFile(dataTransmission);
                     buffer.remove(dataTransmission);
                 }
                 
             }
         }
+    }
+
+    private Boolean transmitAttempt(DataTransmission dataTransmission, Connection connection) {
+        double chance = connection.availability;
+        // Decide if component will fail given a chance. 0.5 = 50% chance of failure, 0.1 = 10% chance
+		Random rand = new Random();
+		double failRandomNumber = rand.nextDouble();
+		if(failRandomNumber <= chance){
+			return true;
+		}
+        eventLog.writeFile(dataTransmission.toString() + " failed to send across network!");
+        return false;
     }
 
     private Connection chooseConnection(DataTransmission dataTransmission) {
